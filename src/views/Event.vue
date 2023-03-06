0d0a9c183bd93605a540913f46cf53eb221a60f2
Dmitry Metelev <redisotschek@gmail.com>
2023-03-07 01:46:54 +0400

    remove unused styles

--- src/views/Event.vue
+++ src/views/Event.vue
@@ -1,29 +1,14 @@
 <template>
-    <div class="about">
-      <event-view />
-    </div>
-  </template>
-  <script>
-  import EventView from "../components/EventView.vue";
-  export default {
-    name: "Event",
-    components: {
-      EventView,
-    },
-  };
-  </script>
-  <style scoped>
-  .show_case {
-    /* background: rgba(0, 0, 0, 0.5); */
-    /* background-blend-mode: multiply; */
-    background-repeat: no-repeat;
-    background-size: cover;
-  }
-  .show_img {
-    object-fit: cover;
-    opacity: 1;
-  }
-  ._img_background {
-    background: rgba(0, 0, 0, 0.5);
-  }
-  </style>
+  <div class="about">
+    <event-view />
+  </div>
+</template>
+<script>
+import EventView from "../components/EventView.vue";
+export default {
+  name: "Event",
+  components: {
+    EventView,
+  },
+};
+</script>
